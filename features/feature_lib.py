20d955979cd6c54d8edf1539b3921fb3c5479166
Stephan Gabler <stephan.gabler@gmail.com>
2012-10-12 16:14:40 +0200

    first commit

---
+++ features/feature_lib.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+library for all the feature analysis and selection stuff
+
+all longer functions should be moved in here to make the individual scripts
+more readable
+
+Created by  on 2012-01-27.
+Copyright (c) 2012. All rights reserved.
+"""
+
+import os, glob
+from collections import defaultdict
+import csv
+from scipy.stats import zscore
+import numpy as np
+import pylab as plt
+
+def read_feature_csvs(features_path):
+    """read the feature CSVs into a dictionary structure"""
+    features = {}
+    molid_idx = 1
+    all_feature_files = glob.glob(os.path.join(features_path, '*.csv'))
+    for feature_file in all_feature_files:
+
+        f_space = os.path.splitext(os.path.basename(feature_file))[0]
+        features[f_space] = defaultdict(dict)
+
+        with open(feature_file) as f:
+            reader = csv.reader(f)
+            header = reader.next()
+
+            # omit the first two columns (id and mol_id)
+            feature_start_idx = 2
+            # this column does not appear in all tables
+            if 'identifiere' in header:
+                feature_start_idx += 1
+
+            for row in reader:
+                mol = row[molid_idx]
+                for f_id in range(feature_start_idx, len(row)):
+                    try:
+                        features[f_space][header[f_id]][mol] = float(row[f_id])
+                    except:
+                        features[f_space][header[f_id]][mol] = 0.
+    return features
+
+
+def normalize_features(features):
+    """z-transform the features to make individual dimensions comparable"""
+    for f_space in features:
+        for feature in features[f_space]:
+            normed = zscore(features[f_space][feature].values())
+            keys = features[f_space][feature].keys()
+            for key, value in zip(keys, normed):
+                features[f_space][feature][int(key)] = value
+    return features
+
+def get_features_for_molids(f_space, molids):
+    """get all features for the given molecule IDs
+
+        result is returnd as array: molecules x features
+    """
+    mol_fspace = [[f_space[f][molid] for f in f_space if molid in f_space[f]]
+                                     for molid in molids]
+    # remove empty entries (features for molid not available)
+    mol_fspace = [elem for elem in mol_fspace if elem]
+    return np.array(mol_fspace)
---
+++ features/isomeres.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+measure for the different features the average similarity vs. stereoisomere similarity
+
+We did this to see which descriptor is susceptible to 3D molecule configurations.
+The problem is that in the experiments of the DoOr database they often used
+mixtures of chemicals
+
+Created by  on 2012-01-27.
+Copyright (c) 2012. All rights reserved.
+"""
+import json, os, csv, glob, json
+import feature_lib as fl
+import pylab as plt
+import numpy as np
+from collections import defaultdict
+import __builtin__
+from scipy.spatial.distance import pdist
+reload(fl)
+
+features_path = '/Users/dedan/projects/master/data/features/'
+out_path = '/Users/dedan/projects/master/results/features'
+N_BINS = 100
+
+# mapping from the CAS number to jan's mol ID
+door2id = json.load(open(os.path.join(features_path, 'door2id.json')))
+
+if os.path.exists(os.path.join(features_path, 'features.json')):
+    print('read previously computed features from: %s JSON' % features_path)
+    features = json.load(open(os.path.join(features_path, 'features.json')))
+else:
+    print('read features from csv, normalize and save them')
+    features = fl.normalize_features(fl.read_feature_csvs(features_path))
+    json.dump(features, open(os.path.join(features_path, 'features.json'), 'w'))
+
+# identify molecules with and without isomeres
+molecules = [molids[0] for molids in door2id.values() if len(molids) == 1]
+isomeres = [molids for molids in door2id.values() if len(molids) > 1]
+
+# create similarity histogram plots for all descriptors
+for f_space in features:
+
+    fig = plt.figure()
+    print 'working on: ', f_space
+    ax = fig.add_subplot(111)
+    mol_fspace = fl.get_features_for_molids(features[f_space], molecules)
+    if np.isnan(mol_fspace).all():
+        print 'only nans in distances'
+        continue
+    ax.hist(pdist(mol_fspace), bins=N_BINS, log=True)
+
+    isomere_distances = np.array([])
+    for isomere in isomeres:
+        iso_fspace = fl.get_features_for_molids(features[f_space], isomere)
+        if iso_fspace.any():
+            isomere_distances = np.hstack([isomere_distances, pdist(iso_fspace)])
+
+    print isomere_distances.shape
+    _, _, patches = ax.hist(isomere_distances, bins=N_BINS, log=True)
+    for patch in patches:
+        plt.setp(patch, color="r")
+    fig.savefig(os.path.join(out_path, f_space + '_histo.png'))
+plt.close('all')
