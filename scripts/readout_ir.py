d9848b8178b52f894330f44f80fc53a6568b797a
Stephan Gabler <stephan.gabler@gmail.com>
2012-10-22 16:37:28 +0200

    write all warnings that might be important into a file

--- scripts/readout_ir.py
+++ scripts/readout_ir.py
@@ -4,20 +4,24 @@
     file to read the results produced by calc_vib
 """
 
-import pickle, os, glob
+import pickle, os, glob, json
+from collections import defaultdict
 
 inpath = '/Users/dedan/projects/master/results/gamess/'
 n_lines_after_warning = 0
 outdict = {}
+problems = defaultdict(list)
 
 uncritical_warnings = ['OLD KEYWORD COORD=CART', 'WARNING, MODE 7']
 
-outfiles=glob.glob(inpath + '*')
+outfiles=glob.glob(inpath + '*.log')
 for outfilename in outfiles:
 
     print 'reading from: ', outfilename
     freq, ir, nonvibrations = [], [], []
     outfile = open(outfilename)
+    molid = os.path.splitext(os.path.basename(outfilename))[0]
+
     for line in outfile:
 
         # print the N lines after a warning
@@ -28,6 +32,7 @@ for outfilename in outfiles:
                 n_lines_after_warning = 0
         if not '* * * WARNING * * *' in line and n_lines_after_warning and line:
             print line
+            problems[molid].append(line)
         if n_lines_after_warning:
             n_lines_after_warning -= 1
 
@@ -48,7 +53,8 @@ for outfilename in outfiles:
     # select only the vibration modes (!!! only if not imaginary !!!)
     freq = [float(freq[i]) for i in range(len(freq)) if not 'I' in freq[i] and not i in nonvibrations]
     ir = [float(ir[i]) for i in range(len(ir)) if not 'I' in ir[i] and not i in nonvibrations]
-    outdict[os.path.splitext(os.path.basename(outfilename))[0]] = zip(freq, ir)
+    outdict[molid] = zip(freq, ir)
 
 pickle.dump(outdict, open(os.path.join(inpath, 'ir.pckl'),'w'))
+json.dump(dict(problems), open(os.path.join(inpath, 'problems.json'),'w'))
 
