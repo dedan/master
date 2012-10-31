90012a1d45e0f27755faeabdd53d5c784066622c
Stephan Gabler <stephan.gabler@gmail.com>
2012-10-31 15:25:36 +0100

    add a run_gaussian script
    it can be used to run the input files created by create_input_files on the soroban cluster at the FU

---
+++ scripts/run_gaussian.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+    script to calc different spectra using GAUSSIAN
+
+    it does not call the programm like the run_gamess script but submits the
+    jobs to the queuing system on the soroban cluster
+
+    the results of this script can be parsed to a simpler structure
+    with readout_gaussian.py
+"""
+
+import os, sys, glob, json
+import subprocess
+from configobj import ConfigObj
+
+config = ConfigObj(sys.argv[1], unrepr=True)
+
+headers = json.load(open(os.path.join(config['module_path'], 'data', 'headers.json')))
+header = headers[config['header']]
+cluster_instr = "%MEM={memory}\n%ProcShared={n_nodes}\n".format(**config)
+
+input_path = os.path.join(config['module_path'], 'data', 'input_files', 'gaussian')
+input_files = glob.glob(os.path.join(input_path, '*.com'))
+
+log = open(os.path.join(config['tmp_folder'], 'gamess_log.log'), 'w')
+wd = os.getcwd()
+os.chdir(config['tmp_folder'])
+
+for input_file in input_files:
+
+    tmp_file = os.path.join(config['tmp_folder'], os.path.basename(input_file))
+    with open(input_file) as f:
+        data = f.read()
+    with open(tmp_file, 'w') as f:
+        f.write("\n".join([cluster_instr, '\n'.join(header), data]))
+
+    # call gamess
+    cmd = 'subg09 ' + tmp_file
+    print '\trunning: %s' % cmd
+    subprocess.call(cmd, shell=True, stdout=log, stderr=log)
+
+os.chdir(wd)
+
+
