bf86d55903ce399cce0e4ca8d5b54918e63fd08a
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-01 14:28:49 +0100

    add stricter error checking to the readout_gamess

--- scripts/readout_gamess.py
+++ scripts/readout_gamess.py
@@ -7,12 +7,13 @@
 import pickle, os, glob, json
 from collections import defaultdict
 
-inpath = '/Users/dedan/projects/master/results/gamess/spectra/gamess_am1/'
+inpath = '/Users/dedan/projects/master/results/spectra/gamess_am1/'
 n_lines_after_warning = 0
 outdict = defaultdict(dict)
-problems = defaultdict(list)
+problems = {'critical': defaultdict(list), 'uncritical': defaultdict(list)}
 
 uncritical_warnings = ['OLD KEYWORD COORD=CART', 'WARNING, MODE 7']
+critical_warnings = ['DATA ARE NOT AVAILABLE FOR ELEMENT']
 
 outfiles=glob.glob(inpath + '*.log')
 for outfilename in outfiles:
@@ -32,9 +33,14 @@ for outfilename in outfiles:
                 n_lines_after_warning = 0
         if not '* * * WARNING * * *' in line and n_lines_after_warning and line:
             print line
-            problems[molid].append(line)
+            problems['uncritical'][molid].append(line)
         if n_lines_after_warning:
             n_lines_after_warning -= 1
+
+        # record critical warning and skip processing for this molecule
+        for cw in critical_warnings:
+            if cw in line:
+                problems['critical'][molid].append(line)
 
         # collect frequencies and intensities
         if 'FREQUENCY' in line and not 'IMAGINARY' in line:
@@ -50,15 +56,19 @@ for outfilename in outfiles:
             assert not nonvibrations
             nonvibrations = range(1, 7)
 
+    if not freq:
+        problems['critical'][molid].append('no FREQUENCY statement found')
+
     # select only the vibration modes (!!! only if not imaginary !!!)
     freq = [f for f in freq if not 'I' in f]
     freq_cut = [float(freq[i]) for i in range(len(freq)) if not i in nonvibrations]
     ir_cut = [float(ir[i]) for i in range(len(ir)) if not i in nonvibrations]
     assert len(freq_cut) == len(ir_cut)
-    assert freq
-    outdict[molid]['freq'] = freq_cut
-    outdict[molid]['ir'] = ir_cut
+    if not molid in problems['critical']:
+        assert freq
+        outdict[molid]['freq'] = freq_cut
+        outdict[molid]['ir'] = ir_cut
 
-pickle.dump(dict(outdict), open(os.path.join(inpath, 'ir.pckl'),'w'))
+pickle.dump(dict(outdict), open(os.path.join(inpath, 'parsed.pckl'),'w'))
 json.dump(dict(problems), open(os.path.join(inpath, 'problems.json'),'w'))
 
