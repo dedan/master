7564f47c9a2238b4af6c84ce410e2e91a97276e3
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-01 14:30:27 +0100

    add a validation script that I use to compare the results of gaussian and gamess

---
+++ validation/compare_gamess_gaussian.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+    we recomputed the IR spectra also using GAUSSIAN
+
+    this script is to compare the results of GAMESS and GAUSSIAN
+"""
+import pickle, os, json
+import numpy as np
+import pylab as plt
+
+gamess_spectra = '/Users/dedan/projects/master/data/spectra/gamess_am1'
+gaussian_spectra = '/Users/dedan/projects/master/data/spectra/gaussian_am1'
+
+gamess = pickle.load(open(os.path.join(gamess_spectra, 'parsed.pckl')))
+gaussian = pickle.load(open(os.path.join(gaussian_spectra, 'parsed.pckl')))
+gamess_problems = json.load(open(os.path.join(gamess_spectra, 'problems.json')))
+gaussian_problems = json.load(open(os.path.join(gaussian_spectra, 'problems.json')))
+
+plt.close('all')
+
+print 'keys'
+print len(set(gamess.keys()).difference(set(gaussian.keys())))
+print sorted(list(set(gamess.keys()).difference(set(gaussian.keys()))))
+print
+
+print 'gamess_problems', sorted(gamess_problems.keys())
+print 'gaussian_problems', sorted(gaussian_problems)
+print len(set(gamess_problems.keys()).difference(set(gaussian_problems.keys())))
+print set(gamess_problems.keys()).difference(set(gaussian_problems.keys()))
+print set(gamess_problems.keys()).intersection(set(gaussian_problems))
+
+for mol in gamess:
+    for dim in ['ir']:
+        gamess[mol][dim] = np.array(gamess[mol][dim]) / np.max(np.array(gamess[mol][dim]))
+for mol in gaussian:
+    for dim in ['ir', 'raman']:
+        gaussian[mol][dim] = np.array(gaussian[mol][dim]) / np.max(np.array(gaussian[mol][dim]))
+
+for_both = list(set(gamess.keys()).intersection(set(gaussian.keys())))
+for i in range(10):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    ax.bar(gaussian[for_both[i]]['freq'], gaussian[for_both[i]]['ir'], edgecolor='b', width=3)
+    plt.hold(True)
+    ax.bar(gamess[for_both[i]]['freq'], -np.array(gamess[for_both[i]]['ir']), edgecolor='r', width=3)
+    ax.set_yticklabels([])
+
+
+
+
+
+
