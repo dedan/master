c71e267e21c588546faa2a0b9ff8d46f719d48c2
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-02 14:58:04 +0100

    first working version of my runner script plus example configs

--- scratch/runner.py
+++ scratch/runner.py
@@ -23,22 +23,71 @@ Copyright (c) 2012. All rights reserved.
 import sys
 import os
 import json
+import pickle
 from master.libs import read_data_lib as rdl
+from master.libs import features as flib
+from collections import defaultdict
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
 reload(rdl)
+reload(flib)
 
 # read from a config file, this might become a job file later
 config = json.load(open(sys.argv[1]))
+door2id = json.load(open(os.path.join(config['data_path'], 'door2id.json')))
+csv_path = os.path.join(config['data_path'], 'response_matrix.csv')
+cas_numbers, glomeruli, rm = rdl.load_response_matrix(csv_path, door2id)
+
 
 # feature related stuff
-if config['features']['type'] == 'conventional':
-    feature_file = os.path.join(config['data_path'],
-                                'conventional_features',
-                                config['features']['descriptor'] + '.csv')
+if config['feature_type'] == 'conventional':
+    feature_file = os.path.join(config['data_path'], 'conventional_features',
+                                config['descriptor'] + '.csv')
     features = rdl.read_feature_csv(feature_file)
-    features = rdl.remove_invalid_features(features)
-    if config['features']['normalize']:
-        features = rdl.normalize_features(features)
 
+elif config['feature_type'] == 'spectral':
+    feature_file = os.path.join(config['data_path'], 'spectral_features',
+                                config['descriptor'], 'parsed.pckl')
+    spectra = pickle.load(open(feature_file))
+    features = flib.get_spectral_features(spectra, config['resolution'],
+                                          spec_type=config['spec_type'],
+                                          kernel_width=config['kernel_width'])
+features = rdl.remove_invalid_features(features)
+if config['normalize_features']:
+    features = rdl.normalize_features(features)
+
+res = defaultdict(dict)
+for glom in config['glomeruli']:
+
+    print glom
+    glom_idx = glomeruli.index(glom)
+
+    # select molecules available for the glomerulus
+    targets , tmp_cas_numbers = rdl.get_avail_targets_for_glom(rm, cas_numbers, glom_idx)
+    molids = [str(door2id[cas_number][0]) for cas_number in tmp_cas_numbers]
+    assert len(molids) == len(targets)
+
+    # for some of them the spectra are not available
+    avail = [i for i in range(len(molids)) if molids[i] in features]
+    targets = [targets[i] for i in avail]
+    data = np.array([features[molids[i]] for i in avail])
+    assert len(targets) == data.shape[0]
+
+    # random forest regression
+    rfr = RandomForestRegressor(n_estimators=config['n_estimators'],
+                                compute_importances=True,
+                                oob_score=True)
+    rfr.fit(data, targets)
+    res[glom]['all_features'] = rfr
+    res[glom]['models'] = []
+
+    # TODO: maybe make this the N_BEST features in order to be method independent
+    for feature_threshold in config['feature_thresholds']:
+        rfr.fit(data[:, rfr.feature_importances_ > feature_threshold], targets)
+        rfr.feature_threshold = feature_threshold
+        res[glom]['models'].append(rfr)
+
+res['config'] = config
 
 
 
