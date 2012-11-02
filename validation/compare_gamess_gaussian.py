cef0d27b16b8a8cf8953e7de66e9f8d72332b89c
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-02 10:01:07 +0100

    compare only the molecules (in gamess vs. gaussian) for which we have CAS numbers

--- validation/compare_gamess_gaussian.py
+++ validation/compare_gamess_gaussian.py
@@ -8,10 +8,12 @@
 import pickle, os, json
 import numpy as np
 import pylab as plt
+import __builtin__
 
 gamess_spectra = '/Users/dedan/projects/master/data/spectra/gamess_am1'
 gaussian_spectra = '/Users/dedan/projects/master/data/spectra/gaussian_am1'
 
+door2id = json.load(open('/Users/dedan/projects/master/data/door2id.json'))
 gamess = pickle.load(open(os.path.join(gamess_spectra, 'parsed.pckl')))
 gaussian = pickle.load(open(os.path.join(gaussian_spectra, 'parsed.pckl')))
 gamess_problems = json.load(open(os.path.join(gamess_spectra, 'problems.json')))
@@ -26,8 +28,8 @@ print
 
 print 'gamess_problems', sorted(gamess_problems.keys())
 print 'gaussian_problems', sorted(gaussian_problems)
-print len(set(gamess_problems.keys()).difference(set(gaussian_problems.keys())))
-print set(gamess_problems.keys()).difference(set(gaussian_problems.keys()))
+print len(set(gamess_problems.keys()).difference(set(gaussian_problems)))
+print set(gamess_problems.keys()).difference(set(gaussian_problems))
 print set(gamess_problems.keys()).intersection(set(gaussian_problems))
 
 for mol in gamess:
@@ -37,15 +39,18 @@ for mol in gaussian:
     for dim in ['ir', 'raman']:
         gaussian[mol][dim] = np.array(gaussian[mol][dim]) / np.max(np.array(gaussian[mol][dim]))
 
-for_both = list(set(gamess.keys()).intersection(set(gaussian.keys())))
+all_with_cas = __builtin__.sum(door2id.values(), [])
+for_both = set(gamess.keys()).intersection(set(gaussian.keys()))
+for_both_with_cas = list(for_both.intersection(all_with_cas))
 for i in range(10):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-
-    ax.bar(gaussian[for_both[i]]['freq'], gaussian[for_both[i]]['ir'], edgecolor='b', width=3)
+    molid = for_both_with_cas[i]
+    ax.bar(gaussian[molid]['freq'], gaussian[molid]['ir'], edgecolor='b', width=3)
     plt.hold(True)
-    ax.bar(gamess[for_both[i]]['freq'], -np.array(gamess[for_both[i]]['ir']), edgecolor='r', width=3)
+    ax.bar(gamess[molid]['freq'], -np.array(gamess[molid]['ir']), edgecolor='r', width=3)
     ax.set_yticklabels([])
+    ax.set_title(molid)
 
 
 
