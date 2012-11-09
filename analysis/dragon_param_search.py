14714ef085f9089490cb83dc7aca84786b56779f
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-09 16:46:26 +0100

    change configuration and results format for parameter search

--- analysis/dragon_param_search.py
+++ analysis/dragon_param_search.py
@@ -1,57 +1,56 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
+'''
 
 Created by  on 2012-01-27.
 Copyright (c) 2012. All rights reserved.
-"""
-import json
-import pickle
-from master.libs import run_lib
-import numpy as np
-import pylab as plt
-reload(run_lib)
+'''
 import glob
 import os
+import json
+from master.libs import run_lib
+import numpy as np
+reload(run_lib)
 
-
+outpath = '/Users/dedan/projects/master/results/param_search/conv_features'
 config = json.load(open('/Users/dedan/projects/master/config/runner_example.json'))
 features = run_lib.prepare_features(config)
 n_features = len(features[features.keys()[0]])
 
-glomeruli = ['Or19a', 'Or22a', 'Or35a', 'Or43b', 'Or67a']
-k_best = [2**i for i in range(10) if 2**i < n_features ]
-forest = [3, 5, 10, 100, 500]
-svr = [0.01, 0.1, 1, 10, 100]
+sc = {
+    'selection': ['linear', 'forest'],
+    'glomeruli': ['Or19a', 'Or22a', 'Or35a', 'Or43b', 'Or67a'],
+    'k_best': [2**i for i in range(10) if 2**i < n_features ],
+    'forest': [3, 5, 10, 100, 500],
+    'svr': [0.01, 0.1, 1, 10, 100]
+}
 
 res = {}
 files = glob.glob('/Users/dedan/projects/master/data/conventional_features/*.csv')
 for f in files:
     desc = os.path.splitext(os.path.basename(f))[0]
+    if os.path.exists(os.path.join(outpath, desc + '.json')):
+        print 'skip {}, already exists'.format(desc)
+        continue
+
     config['features']['descriptor'] = desc
     print 'working on: ', desc
 
-    for selection in ["linear", "forest"]:
+    for selection in sc['selection']:
         print selection
         res[selection] = {}
         config['feature_selection']['method'] = selection
-        for glomerulus in glomeruli:
+        for glomerulus in sc['glomeruli']:
             res[selection][glomerulus] = {}
             config['glomerulus'] = glomerulus
-            for k_b in k_best:
+            for k_b in sc['k_best']:
                 res[selection][glomerulus][k_b] = {}
                 config['feature_selection']['k_best'] = k_b
-                for i in range(len(forest)):
-                    config['methods']['svr']['C'] = svr[i]
-                    config['methods']['svr_ens']['C'] = svr[i]
-                    config['methods']['forest']['max_depth'] = forest[i]
+                for i in range(len(sc['forest'])):
+                    config['methods']['svr']['C'] = sc['svr'][i]
+                    config['methods']['svr_ens']['C'] = sc['svr'][i]
+                    config['methods']['forest']['max_depth'] = sc['forest'][i]
                     tmp_res = run_lib.run_runner(config, features)
-                    res[selection][glomerulus][k_b][forest[i]] = tmp_res
-    pickle.dump(res, open(desc + '.pckl', 'w'))
-
-
-
-
-
-
-
+                    tmp_res['n_features'] = n_features
+                    res[selection][glomerulus][k_b][i] = tmp_res
+    json.dump({'sc': sc, 'res': res}, open(os.path.join(outpath  + desc + '.json'), 'w'))
--- analysis/plot/plot_dragon.py
+++ analysis/plot/plot_dragon.py
@@ -5,53 +5,47 @@
 Created by  on 2012-01-27.
 Copyright (c) 2012. All rights reserved.
 """
-
+import glob
 import sys
 import os
 import json
-import pickle
 import numpy as np
 import pylab as plt
 from master.libs import run_lib
 
-
-config = json.load(open('/Users/dedan/projects/master/config/runner_example.json'))
-features = runner.prepare_features(config)
-n_features = len(features[features.keys()[0]])
-
-glomeruli = ['Or19a', 'Or22a', 'Or35a', 'Or43b', 'Or67a']
-k_best = [2**i for i in range(10) if 2**i < n_features ]
-forest = [3, 5, 10, 100, 500]
-svr = [0.01, 0.1, 1, 10, 100]
-
+inpath = '/Users/dedan/projects/master/results/param_search/conv_features'
 
 plt.close('all')
-res = pickle.load(open('test.pckl'))
+for f_name in glob.glob(os.path.join(inpath, "*.json")):
 
-for method in ['svr', 'svr_ens', 'forest']:
-    fig = plt.figure()
-    fig.suptitle(method)
-    for i_sel, selection in enumerate(res):
-        for i_glom, glom in enumerate(res[selection]):
+    js = json.load(open(f_name))
+    res = js['res']
+    sc = js['sc']
 
-            ax = fig.add_subplot(len(res), len(glomeruli), i_sel * len(glomeruli) + i_glom + 1)
-            mat = np.zeros((len(k_best), len(svr)))
-            for j, k_b in enumerate(k_best):
-                for i in range(len(forest)):
-                    mat[j,i] = res[selection][glom][k_b][forest[i]][method]['gen_score']
+    for method in ['svr', 'svr_ens', 'forest']:
+        fig = plt.figure()
+        fig.suptitle(method)
+        for i_sel, selection in enumerate(sc['selection']):
+            for i_glom, glom in enumerate(res[selection]):
 
-            res[selection][glom]['mat'] = mat
-            ax.imshow(mat, interpolation='nearest')
-            if i_glom == 0:
-                ax.set_yticks(range(len(k_best)))
-                ax.set_yticklabels(k_best)
-            else:
-                ax.set_yticks([])
+                ax = fig.add_subplot(len(res), len(sc['glomeruli']), i_sel * len(sc['glomeruli']) + i_glom + 1)
+                mat = np.zeros((len(sc['k_best']), len(sc['svr'])))
+                for j, k_b in enumerate(sc['k_best']):
+                    for i in range(len(sc['forest'])):
+                        mat[j,i] = res[selection][glom][str(k_b)][str(i)][method]['gen_score']
 
-            ax.set_xticks(range(len(svr)))
-            if 'svr' in method:
-                ax.set_xticklabels(svr, rotation='45')
-            else:
-                ax.set_xticklabels(forest, rotation='45')
-            ax.set_xlabel('max: %.2f' % np.max(mat))
+                res[selection][glom]['mat'] = mat
+                ax.imshow(mat, interpolation='nearest')
+                if i_glom == 0:
+                    ax.set_yticks(range(len(sc['k_best'])))
+                    ax.set_yticklabels(sc['k_best'])
+                else:
+                    ax.set_yticks([])
+
+                ax.set_xticks(range(len(sc['svr'])))
+                if 'svr' in method:
+                    ax.set_xticklabels(sc['svr'], rotation='45')
+                else:
+                    ax.set_xticklabels(sc['forest'], rotation='45')
+                ax.set_xlabel('max: %.2f' % np.max(mat))
 plt.show()
