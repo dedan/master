c239a53454655450b944bc7eb25387f96a313e07
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-14 11:45:25 +0100

    only do the parameter search for glomeruli which haven't been computed before

--- analysis/dragon_param_search.py
+++ analysis/dragon_param_search.py
@@ -17,21 +17,23 @@ config = json.load(open('/Users/dedan/projects/master/config/runner_example.json
 
 sc = {
     'selection': ['linear', 'forest'],
-    'glomeruli': ['Or19a', 'Or22a', 'Or35a', 'Or43b', 'Or67a'],
+    'glomeruli': ['Or19a', 'Or22a', 'Or35a', 'Or43b', 'Or67a', 'Or9a'],
     'forest': [3, 5, 10, 100, 500],
     'svr': [0.01, 0.1, 1, 10, 100]
 }
 
-res = {}
 files = glob.glob('/Users/dedan/projects/master/data/conventional_features/*.csv')
 for f in files:
     desc = os.path.splitext(os.path.basename(f))[0]
-    if os.path.exists(os.path.join(outpath, desc + '.json')):
-        print 'skip {}, already exists'.format(desc)
-        continue
-
     config['features']['descriptor'] = desc
     config['features']['type'] = 'conventional'
+
+    # if result file already exists, load it to append new glomeruli
+    if os.path.exists(os.path.join(outpath, desc + '.json')):
+        print('load existing results from: {}'.format(desc))
+        res = json.load(open(os.path.join(outpath, desc + '.json')))["res"]
+    else:
+        res = {sel: {} for sel in sc['selection']}
 
     # load the features
     features = run_lib.prepare_features(config)
@@ -42,9 +44,11 @@ for f in files:
 
     for selection in sc['selection']:
         print selection
-        res[selection] = {}
         config['feature_selection']['method'] = selection
         for glomerulus in sc['glomeruli']:
+            if glomerulus in res[selection]:
+                print('search for {} already done'.format(glomerulus))
+                continue
             res[selection][glomerulus] = {}
             config['glomerulus'] = glomerulus
             for k_b in sc['k_best']:
@@ -57,4 +61,5 @@ for f in files:
                     tmp_res = run_lib.run_runner(config, features)
                     tmp_res['n_features'] = n_features
                     res[selection][glomerulus][k_b][i] = tmp_res
+            print('param search for {} done'.format(glomerulus))
     json.dump({'sc': sc, 'res': res}, open(os.path.join(outpath, desc + '.json'), 'w'))
