c180633473ba8d174fa91a436172ce514f3a594b
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-21 19:15:04 +0100

    use the runner only on a single run, better for updating single messed up runs

--- scripts/add_runner_config_content.py
+++ scripts/add_runner_config_content.py
@@ -15,17 +15,16 @@ runner_example_path = os.path.join(os.path.dirname(__file__),
                                    '..', 'config', 'runner_example.json')
 runner_example_config = json.load(open(runner_example_path))
 
-path = '/Users/dedan/mnt/numbercruncher/results/param_search'
-for folder in os.listdir(path):
+folder = '/Users/dedan/mnt/numbercruncher/results/param_search/conv_features'
 
-    filenames = glob.glob(os.path.join(path, folder, '*.json'))
-    for filename in filenames:
-        print filename
-        with open(filename) as f:
-            res = json.load(f)
-        res['sc']['runner_config_content'] = runner_example_config
-        with open(filename, 'w') as f:
-            json.dump(res, f)
+filenames = glob.glob(os.path.join(folder, '*.json'))
+for filename in filenames:
+    print filename
+    with open(filename) as f:
+        res = json.load(f)
+    res['sc']['runner_config_content'] = runner_example_config
+    with open(filename, 'w') as f:
+        json.dump(res, f)
 
 
 
