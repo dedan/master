70f641317819dfb6acfaaa368ae172a36e208146
Stephan Gabler <stephan.gabler@gmail.com>
2012-11-23 16:02:32 +0100

    finish randomization test

--- validation/bootstrap_test.py
+++ validation/bootstrap_test.py
@@ -15,62 +15,72 @@ Copyright (c) 2012. All rights reserved.
 import json
 import sys
 import os
+import copy
 from master.libs import run_lib
 from master.libs import read_data_lib as rdl
 from master.libs import features_lib as flib
 import numpy as np
 import pylab as plt
+reload(run_lib)
 
-n_repetitions = 100
+plt.close('all')
+n_repetitions = 10
 desc = 'haddad_desc'
 selection = 'linear'
 method = 'svr'
 # TODO: maybe search for a good glomerulus
-glom = 'Or19a'
+glom = "Or22a"
 path = '/Users/dedan/mnt/numbercruncher/results/param_search/conv_features/'
 
 # get the best parameters from the parameter search
 search_res, max_overview, sc = rdl.read_paramsearch_results(path, [method])
 
 # get the base config
-config = sc['runner_config_content']
-config['features']['descriptor'] = desc
+config_orig = sc['runner_config_content']
+config_orig['features']['descriptor'] = desc
 mat = search_res[desc][selection][glom][method]
 k_best_idx = np.argmax(np.max(mat, axis=1))
 reg_idx = np.argmax(np.max(mat, axis=0))
-print sc
-print 'svr' in sc
 if 'svr' in method:
-    config['methods'][method]['C'] = sc['svr'][reg_idx]
+    config_orig['methods'][method]['C'] = sc['svr'][reg_idx]
 else:
-    config[method]['n_estimators'] = sc['forest'][reg_idx]
-config['feature_selection']['method'] = selection
+    config_orig[method]['n_estimators'] = sc['forest'][reg_idx]
+config_orig['feature_selection']['method'] = selection
+config_orig['glomerulus'] = glom
 
 
-config['randomization_test'] = False
-features = run_lib.prepare_features(config)
-data, targets = run_lib.load_data_targets(config, features)
-sel_scores = run_lib.get_selection_score(config, data, targets)
-data = flib.select_k_best(data, sel_scores, sc['k_best'][k_best_idx])
-true_res = run_lib.run_runner(config, data, targets)
+for method in config_orig['methods']:
 
-# add shuffle data to the config and run the runner for N times
-config['randomization_test'] = True
-rand_res = []
-for i in range(n_repetitions):
+    print method
+    config = copy.deepcopy(config_orig)
+    for m in list(config['methods'].keys()):
+        if not method in m:
+            del(config['methods'][m])
+    config['randomization_test'] = False
+    features = run_lib.prepare_features(config)
     data, targets = run_lib.load_data_targets(config, features)
     sel_scores = run_lib.get_selection_score(config, data, targets)
     data = flib.select_k_best(data, sel_scores, sc['k_best'][k_best_idx])
-    tmp_res = run_lib.run_runner(config, data, targets)
-    rand_res.append(tmp_res['forest']['gen_score'])
+    true_res = run_lib.run_runner(config, data, targets)
 
-# readout the resulting files and analyze them
+    # add shuffle data to the config and run the runner for N times
+    config['randomization_test'] = True
+    rand_res = []
+    fig = plt.figure()
+    fig.suptitle(method)
+    ax = fig.add_subplot(211)
+    for i in range(n_repetitions):
+        orig_data, orig_targets = run_lib.load_data_targets(config, features)
+        data, targets = orig_data.copy(), orig_targets.copy()
+        sel_scores = run_lib.get_selection_score(config, data, targets)
+        data = flib.select_k_best(data, sel_scores, sc['k_best'][k_best_idx])
+        tmp_res = run_lib.run_runner(config, data, targets, get_models=True)
+        rand_res.append(tmp_res[method]['gen_score'])
+        ax.plot(orig_targets, tmp_res[method]['model'].predict(orig_data), 'x')
 
-# create a plot of the resulting distribution and the original value
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.hist(rand_res)
-plt.hold(True)
-ax.plot([true_res['forest']['gen_score']], [1], 'r*')
-
+    # create a plot of the resulting distribution and the original value
+    ax = fig.add_subplot(212)
+    ax.hist(rand_res)
+    ax.plot([true_res[method]['gen_score']], [1], 'r*')
+    plt.show()
 
