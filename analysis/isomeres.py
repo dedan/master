7e9bff5c67514332bed0f358994af28e6dd86e40
Stephan Gabler <stephan.gabler@gmail.com>
2012-12-03 10:47:59 +0100

    fix the isomers script
    it used an old data structure that I don't want to use anymore.

--- analysis/isomeres.py
+++ analysis/isomeres.py
@@ -12,6 +12,8 @@ Copyright (c) 2012. All rights reserved.
 """
 import json, os, csv, glob, json
 import master.libs.read_data_lib as rdl
+import master.libs.features_lib as flib
+from master.libs import run_lib
 import pylab as plt
 import numpy as np
 from collections import defaultdict
@@ -19,26 +21,40 @@ import __builtin__
 from scipy.spatial.distance import pdist
 reload(rdl)
 
+config = {
+         "features":
+            {
+              "type": "conventional"
+            , "normalize": True
+            , "normalize_samples": False
+            , "properties_to_add": []
+            },
+         "data_path": '/Users/dedan/projects/master/data'
+         }
 base_path = '/Users/dedan/projects/master/'
 out_path = os.path.join(base_path, 'results', 'isomeres')
-data_path = os.path.join(base_path, 'data')
+data_path = os.path.join(base_path, 'data', 'conventional_features')
 N_BINS = 100
 
 # mapping from the CAS number to jan's mol ID
-door2id = json.load(open(os.path.join(data_path, 'door2id.json')))
-features = json.load(open(os.path.join(data_path, 'features.json')))
+door2id = json.load(open(os.path.join(data_path, '..', 'door2id.json')))
 
 # identify molecules with and without isomeres
 molecules = [molids[0] for molids in door2id.values() if len(molids) == 1]
 isomeres = [molids for molids in door2id.values() if len(molids) > 1]
 
 # create similarity histogram plots for all descriptors
-for f_space in features:
+feature_files = glob.glob(os.path.join(data_path, '*.csv'))
+for f in feature_files:
+
+    desc = os.path.splitext(os.path.basename(f))[0]
+    config['features']['descriptor'] = desc
+    features = run_lib.prepare_features(config)
 
     fig = plt.figure()
-    print 'working on: ', f_space
+    print 'working on: ', desc
     ax = fig.add_subplot(111)
-    mol_fspace, _ = rdl.get_features_for_molids(features[f_space], molecules)
+    mol_fspace, _ = flib.get_features_for_molids(features, molecules)
     assert not (mol_fspace == -999).any()
     mol_distances = pdist(mol_fspace)
     bins = np.linspace(0, max(mol_distances), num=N_BINS)
@@ -46,7 +62,7 @@ for f_space in features:
 
     isomere_distances = np.array([])
     for isomere in isomeres:
-        iso_fspace, _ = rdl.get_features_for_molids(features[f_space], isomere)
+        iso_fspace, _ = flib.get_features_for_molids(features, isomere)
         assert not (iso_fspace == -999).any()
         if iso_fspace.any():
             isomere_distances = np.hstack([isomere_distances, pdist(iso_fspace)])
