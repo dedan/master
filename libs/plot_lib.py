eb485bd79a1ded95358669e62092510598ec9668
Stephan Gabler <stephan.gabler@gmail.com>
2012-12-04 11:49:25 +0100

    same ylim in descriptor comparison plot

--- libs/plot_lib.py
+++ libs/plot_lib.py
@@ -32,6 +32,7 @@ def structure_plot(fig, molids, activations=None):
 
 def new_descriptor_performance_plot(fig, max_overview, sc):
     """compare performance of different descriptors for several glomeruli"""
+    n_plots = len(max_overview) * len(max_overview.values()[0])
     for i_meth, method in enumerate(max_overview):
 
         for i_sel, selection in enumerate(max_overview[method]):
@@ -44,7 +45,7 @@ def new_descriptor_performance_plot(fig, max_overview, sc):
             data = data[:, sort_x]
 
             plot_x = i_sel * len(max_overview) + i_meth + 1
-            ax = fig.add_subplot(1, 6, plot_x)
+            ax = fig.add_subplot(1, n_plots, plot_x)
             ax.set_title('{}_{}.'.format(method, selection))
             ax.bar((np.arange(len(desc_names)) - 0.5) * 3,
                    np.mean(data, axis=1),
@@ -53,6 +54,7 @@ def new_descriptor_performance_plot(fig, max_overview, sc):
             ax.spines['right'].set_visible(False)
             ax.get_xaxis().tick_bottom()
             ax.get_yaxis().tick_left()
+            ax.set_ylim([0, 0.8])
             ax.set_xticks(np.arange(len(desc_names)) * 3)
             ax.set_xticklabels([desc_names[i] for i in sort_y], rotation='90', fontsize=6)
             if plot_x == 1:
