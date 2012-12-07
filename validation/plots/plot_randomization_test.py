51d3463ca89af632d7b4ab3e913b72010661f3fe
Stephan Gabler <stephan.gabler@gmail.com>
2012-12-07 15:31:15 +0100

    separate randomization test and producing the numbers and plotting

---
+++ validation/plots/plot_randomization_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+plot the results of a randomization test
+
+Created by  on 2012-01-27.
+Copyright (c) 2012. All rights reserved.
+"""
+
+import sys
+import os
+import numpy as np
+import pylab as plt
+
+
+#     # create a plot of the resulting distribution and the original value
+#     ax = fig.add_subplot(len(glomeruli), 1, i+1)
+#     ax.hist(rand_res)
+#     ax.plot([true_res], [1], 'r*')
+#     ax.set_ylabel(glom)
+#     ax.set_xlim([-2, 0.8])
+# fig.subplots_adjust(h_space=0.3)
+# fig.savefig(os.path.join(outpath, name + '.png'))
+# plt.show()
+
