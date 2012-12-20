fix generalization score computation for the single SVR and add a few assertions

libs/learning_lib.py
@@ -40,30 +40,35 @@ class StratifiedResampling(object):
                 train_idx.extend(sample)
             assert len(train_idx) == len(self.binned_targets)
             test_idx = [j for j in range(len(self.targets)) if not j in train_idx]
+            assert len(set(train_idx).intersection(test_idx)) == 0
             yield train_idx, test_idx
 
 
 class MySVR(SVR):
     """docstring for MySVR"""
-    def __init__(self, cross_val=True, n_folds=10, stratified=False, **kwargs):
+    def __init__(self, cross_val=True, n_folds=10, **kwargs):
         super(MySVR, self).__init__(**kwargs)
         self.kwargs = kwargs
         self.cross_val = cross_val
         self.n_folds = n_folds
         self.r2_score_ = None
         self.oob_score_ = None
-        self.stratified = stratified
 
     def fit(self, data, targets):
         """docstring for fit"""
+        assert data.shape[0] == len(targets)
+        print "fit data shape: {}".format(data.shape)
+
         super(MySVR, self).fit(data, targets)
         tmp_svr = SVR(**self.kwargs)
         if self.cross_val:
             kf = StratifiedResampling(targets, self.n_folds)
-            predictions = np.zeros(len(targets))
+            all_predictions, all_targets = [], []
             for train, test in kf:
-                predictions[test] = tmp_svr.fit(data[train], targets[train]).predict(data[test])
-            self.r2_score_ = r2_score(targets, predictions)
+                all_predictions.extend(tmp_svr.fit(data[train], targets[train])
+                                              .predict(data[test]))
+                all_targets.extend(targets[test])
+            self.r2_score_ = r2_score(all_targets, all_predictions)
             self.oob_score_ = self.r2_score_
         return self
 
@@ -83,6 +88,7 @@ class SVREnsemble(object):
 
     def fit(self, data, targets):
         """docstring for fit"""
+        assert data.shape[0] == len(targets)
 
         if self.stratified:
             sr = StratifiedResampling(targets, self.n_estimators)