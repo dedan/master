7182736df9b0fcbf63207c9fbdcd3a56099a3e44
Stephan Gabler <stephan.gabler@gmail.com>
2013-01-04 16:08:23 +0100

    make the high resolution example more minimal, simple and use even higher resolution
    --> still giving high generalization score

--- scratch/high_res_spectral_fitting.py
+++ scratch/high_res_spectral_fitting.py
@@ -25,25 +25,13 @@ config = {
         "method": "linear"
     },
     "features": {
-        "normalize": True,
-        "normalize_samples": False,
-        "type": "spectral",
-        "resolution": 0.1,
-        "spec_type": "ir",
-        "use_intensity": True,
-        "kernel_width": 1,
-        "bin_width": 1,
-        "properties_to_add": []
+        'normalize_samples': False
     },
     "methods": {
         "svr": {
             "cross_val": True,
             "C": 1.0,
             "n_folds": 10
-        # },
-        # "svr_ens": {
-        #     "n_estimators": 500,
-        #     "oob_score": True
         }
     },
     "data_path": "/Users/dedan/projects/master/data",
@@ -51,24 +39,23 @@ config = {
     "randomization_test": False
 }
 
-
-features = run_lib.prepare_features(config)
-data, targets, molids = run_lib.load_data_targets(config, features)
+# get the molids
+spectra = pickle.load(open('/Users/dedan/projects/master/data/spectral_features/large_base/parsed.pckl'))
+_, targets, molids = run_lib.load_data_targets(config, {str(i): [] for i in spectra.keys()})
 
 # some molids map to two CAS numbers for some molecules, use only first
 first_molids_idx = sorted([molids.index(m) for m in set(molids)])
 targets = targets[first_molids_idx]
-data = data[first_molids_idx]
+data_orig = data_orig[first_molids_idx]
 molids = [molids[i] for i in first_molids_idx]
 
-
-# freqs = {k: v for k, v in features.items() if k in molids}
-# data = flib._place_waves_in_vector(freqs, 0.01, True, 'ir')
-assert len(molids) == len(targets) == data.shape[0]
-
+# place the intensity values in a high-resolution vector
+freqs = {k: v for k, v in spectra.items() if k in molids}
+data_orig = flib._place_waves_in_vector(freqs, 0.01, True, 'ir')
+assert len(molids) == len(targets) == data_orig.shape[0]
 
 # fit model
-sel_scores = run_lib.get_selection_score(config, data, targets)
-data = flib.select_k_best(data, sel_scores, 2**9)
+sel_scores = run_lib.get_selection_score(config, data_orig, targets)
+data = flib.select_k_best(data_orig, sel_scores, 2**9)
 tmp_res = run_lib.run_runner(config, data, targets, get_models=True)
 model = tmp_res['svr']['model']
