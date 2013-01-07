afdbba8a24f5fe9d2322b14b693e5e88dcebd46d
Stephan Gabler <stephan.gabler@gmail.com>
2013-01-07 13:54:45 +0100

    last version of the high_res_spectral file that helped my tracing down the bug

--- validation/high_res_spectral_fitting.py
+++ validation/high_res_spectral_fitting.py
@@ -46,7 +46,6 @@ _, targets, molids = run_lib.load_data_targets(config, {str(i): [] for i in spec
 # some molids map to two CAS numbers for some molecules, use only first
 first_molids_idx = sorted([molids.index(m) for m in set(molids)])
 targets = targets[first_molids_idx]
-data_orig = data_orig[first_molids_idx]
 molids = [molids[i] for i in first_molids_idx]
 
 # place the intensity values in a high-resolution vector
@@ -54,8 +53,12 @@ freqs = {k: v for k, v in spectra.items() if k in molids}
 data_orig = flib._place_waves_in_vector(freqs, 0.01, True, 'ir')
 assert len(molids) == len(targets) == data_orig.shape[0]
 
+# randomization
+np.random.seed()
+map(np.random.shuffle, data_orig.T)
+
 # fit model
 sel_scores = run_lib.get_selection_score(config, data_orig, targets)
 data = flib.select_k_best(data_orig, sel_scores, 2**9)
 tmp_res = run_lib.run_runner(config, data, targets, get_models=True)
-model = tmp_res['svr']['model']
+print tmp_res['svr']['model'].oob_score_
