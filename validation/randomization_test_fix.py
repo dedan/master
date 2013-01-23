c8cc44e79fa0335e54993ea2e8ce27412ae6bc6e
Stephan Gabler <stephan.gabler@gmail.com>
2013-01-23 15:07:04 +0100

    because we found that we can also use the forest without parameter search --> only fixed randomization test

--- validation/randomization_test_fix.py
+++ validation/randomization_test_fix.py
@@ -9,6 +9,9 @@
     target values. It would only perform equally well if the unshuffled
     observations were already meaningless (without target related structure)
 
+    this version of the script is for the case where we don't do any
+    parameter search, instead we just train the model with fixed values
+
 Created by  on 2012-01-27.
 Copyright (c) 2012. All rights reserved.
 """
@@ -27,29 +30,26 @@ plt.close('all')
 # repeat paramsearch N times with randomization_test set to true
 # only for one descriptor
 # record all the results from the different runs
-res = {}
 sc = json.load(open(sys.argv[1]))
 config = sc['runner_config_content']
 config['data_path'] = os.path.join(os.path.dirname(__file__), '..', 'data')
 json.dump(sc, open(os.path.join(sc['outpath'], 'sc_config.json'), 'w'))
 assert len(config['methods']) == 1  # only one method a time
+method = config['methods'].keys()[0]
 
 # load the features
 features = run_lib.prepare_features(config)
 n_features = len(features[features.keys()[0]])
-max_expo = int(np.floor(np.log2(n_features)))
-sc['k_best'] = [2**i for i in range(max_expo)] + [n_features]
+config['feature_selection']['k_best'] = n_features
 
 print('true run')
 config['randomization_test'] = False
-if 'svr' in config['methods']:
-    config['methods']['svr']['n_folds'] = 500
+res = {}
 for glomerulus in sc['glomeruli']:
     config['glomerulus'] = glomerulus
-    if not glomerulus in res:
-        res[glomerulus] = {}
-    res[glomerulus] = run_lib.do_paramsearch(sc, config, features, res[glomerulus])
-    print('param search for {} done'.format(glomerulus))
+    data, targets, molids = run_lib.load_data_targets(config, features)
+    tmp_res = run_lib.run_runner(config, data, targets, get_models=True)
+    res[glomerulus] = tmp_res[method]['gen_score']
 json.dump(res, open(os.path.join(sc['outpath'], 'true.json'), 'w'))
 
 # add shuffle data to the config and run the runner for N times
@@ -59,10 +59,10 @@ for i in range(sc['n_repetitions']):
     res = {}
     for glomerulus in sc['glomeruli']:
         config['glomerulus'] = glomerulus
-        if not glomerulus in res:
-            res[glomerulus] = {}
-        res[glomerulus] = run_lib.do_paramsearch(sc, config, features, res[glomerulus])
-        print('param search for {} done'.format(glomerulus))
+        data, targets, molids = run_lib.load_data_targets(config, features)
+        tmp_res = run_lib.run_runner(config, data, targets, get_models=True)
+        res[glomerulus] = tmp_res[method]['gen_score']
+    json.dump(res, open(os.path.join(sc['outpath'], 'true.json'), 'w'))
     run_name = 'run_' + time.strftime("%d%m%Y_%H%M%S", time.localtime())
     json.dump(res, open(os.path.join(sc['outpath'], run_name + '.json'), 'w'))
 
