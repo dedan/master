cac4c9610b85b8cbcd5c154c496a94cc15c5e3f0
Stephan Gabler <stephan.gabler@gmail.com>
2013-01-24 10:41:39 +0100

    fit randomization test plot to the data structure when no parameter search is done and compute P values for the test

--- validation/plots/plot_randomization_test.py
+++ validation/plots/plot_randomization_test.py
@@ -17,40 +17,43 @@ from master.libs import read_data_lib as rdl
 from master.libs import utils
 
 res = {}
-method = 'svr_ens'
+method = 'forest'
+inpath = '/Users/dedan/projects/master/results/validation/svrlin_haddad'
 
-inpath = '/Users/dedan/projects/master/results/validation/lin_svrens'
 outpath = os.path.join(inpath, 'plots')
 if not os.path.exists(outpath):
     os.mkdir(outpath)
-true_res_file = os.path.join(inpath, 'true.json')
-true_res = json.load(open(true_res_file))
-for glom in true_res:
-    mat = rdl.get_search_matrix(true_res[glom], method)
+
+true_res = json.load(open(os.path.join(inpath, 'true.json')))
+for glom, value in true_res.items():
     if not glom in res:
         res[glom] = {"rand_res_dist": []}
-
-    res[glom]['true_res'] = np.max(mat)
+    res[glom]['true_res'] = value
 
 rand_res_files = glob.glob(os.path.join(inpath, 'run_*.json'))
 for rand_res_file in rand_res_files:
     rand_res = json.load(open(rand_res_file))
-    for glom in rand_res:
-        mat = rdl.get_search_matrix(rand_res[glom], method)
-        res[glom]['rand_res_dist'].append(np.max(mat))
+    for glom, value in rand_res.items():
+        res[glom]['rand_res_dist'].append(value)
 
+
+# compute statistics
+for r in res.values():
+    r['p'] = np.sum(np.array(r['rand_res_dist']) > r['true_res']) / float(len(r['rand_res_dist']))
+
+# create a plot of the resulting distribution and the original value
 fig = plt.figure(figsize=(8,4))
 for i, glom in enumerate(res):
 
-    # create a plot of the resulting distribution and the original value
     ax = fig.add_subplot(len(res), 1, i+1)
     ax.hist(res[glom]['rand_res_dist'])
     ax.plot([res[glom]['true_res']], [5], 'r*')
-    ax.set_ylabel(glom, rotation='0')
+    ax.set_ylabel('{} - {:.3f}'.format(glom, res[glom]['p']), rotation='0')
     ax.set_yticks([])
     ax.set_xlim([-0.4, 0.8])
     if not i == len(res) - 1:
         ax.set_xticks([])
+    print('{} - p: {:.5f}'.format(glom, res[glom]['p']))
 fig.subplots_adjust(hspace=0.3)
 fig.savefig(os.path.join(outpath, 'rand_test.png'))
 if utils.run_from_ipython():
