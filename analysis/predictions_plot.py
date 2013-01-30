343c1e61e99ecbce3ba397873ef1c315d6c1c701
Stephan Gabler <stephan.gabler@gmail.com>
2013-01-30 17:26:02 +0100

    compute correlations between predictions of different descriptors in the predictions_plot

--- analysis/predictions_plot.py
+++ analysis/predictions_plot.py
@@ -19,62 +19,72 @@ import matplotlib.gridspec as gridspec
 reload(rl)
 
 plt.close('all')
-configs = json.load(open(sys.argv[1]))
-glom = sys.argv[2]
 outpath = '/Users/dedan/projects/master/results/predict'
 data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
-for config in configs.values():
-    config.update({'glomerulus': glom, 'data_path': data_path})
+configs = json.load(open(sys.argv[1]))
+if len(sys.argv) < 3:
+    gloms = json.load(open(os.path.join(data_path, 'used_glomeruli.json')))
+else:
+    gloms = [sys.argv[2]]
 
-res = {k: {'features': rl.prepare_features(config)} for k, config in configs.items()}
-all_mols = [r['features'].keys() for r in res.values()]
-mol_intersection = set(all_mols[0]).intersection(*all_mols[1:])
+correlations = {g: [] for g in gloms}
+for glom in gloms:
+    for config in configs.values():
+        config.update({'glomerulus': glom, 'data_path': data_path})
 
-for conf_name, config in configs.items():
-    print('working on model: {}'.format(conf_name))
+    res = {k: {'features': rl.prepare_features(config)} for k, config in configs.items()}
+    all_mols = [r['features'].keys() for r in res.values()]
+    mol_intersection = set(all_mols[0]).intersection(*all_mols[1:])
 
-    # load features
-    print 'preparing features..'
-    data, targets, molids = rl.load_data_targets(config, res[conf_name]['features'])
-    if config['feature_selection']['k_best'] == 'max':
-        config['feature_selection']['k_best'] = data.shape[1]
+    for conf_name, config in configs.items():
+        print('working on model: {}'.format(conf_name))
 
-    # fit model
-    print("use {} molecules for training".format(data.shape[0]))
-    tmp_res = rl.run_runner(config, data, targets, get_models=True)
-    method = config['methods'].keys()[0]
-    to_predict = np.array([res[conf_name]['features'][molid] for molid in mol_intersection])
-    res[conf_name] = tmp_res[method]['model'].predict(to_predict)
-    print('model genscore: {:.2f}\n'.format(tmp_res[method]['gen_score']))
+        # load features
+        print 'preparing features..'
+        data, targets, molids = rl.load_data_targets(config, res[conf_name]['features'])
+        if config['feature_selection']['k_best'] == 'max':
+            config['feature_selection']['k_best'] = data.shape[1]
 
-
-mn = configs.keys()
-gs = gridspec.GridSpec(len(mn)-1, len(mn)-1)
-gs.update(wspace=0.2, hspace=0.2)
-for m1, m2 in it.combinations(mn, 2):
-    ax = plt.subplot(gs[mn.index(m1), mn.index(m2)-1])
-    ax.plot(res[m2], res[m1], 'x', color='#53777A')
-    ax.plot([0, 1], [0, 1], color='0.5')
-    ax.set_xlim([-0.2, 1])
-    ax.set_ylim([-0.2, 1])
-    if mn.index(m1) == (mn.index(m2)-1):
-        ax.set_ylabel(m1)
-    if mn.index(m1) == 0:
-        ax.set_title(m2)
-    if not (mn.index(m1) == 0 and mn.index(m2) == 1):
-        ax.set_yticks([])
-    if not (mn.index(m1) == (len(mn)-2) and mn.index(m2) == (len(mn)-1)):
-        ax.set_xticks([])
-plt.savefig(os.path.join(outpath, glom + '_prediction_comparison.png'))
-
-if utils.run_from_ipython():
-    plt.show()
-
-with open(os.path.join(outpath, glom + '_predictions.csv'), 'w') as f:
-    f.write(',{}\n'.format(','.join(res.keys())))
-    for i, molid in enumerate(mol_intersection):
-        f.write(molid + ',')
-        f.write(','.join([str(r[i]) for r in res.values()]))
-        f.write('\n')
+        # fit model
+        print("use {} molecules for training".format(data.shape[0]))
+        tmp_res = rl.run_runner(config, data, targets, get_models=True)
+        method = config['methods'].keys()[0]
+        to_predict = np.array([res[conf_name]['features'][molid] for molid in mol_intersection])
+        res[conf_name] = tmp_res[method]['model'].predict(to_predict)
+        print('model genscore: {:.2f}\n'.format(tmp_res[method]['gen_score']))
 
 
+    mn = configs.keys()
+    gs = gridspec.GridSpec(len(mn)-1, len(mn)-1)
+    gs.update(wspace=0.2, hspace=0.2)
+    for m1, m2 in it.combinations(mn, 2):
+        ax = plt.subplot(gs[mn.index(m1), mn.index(m2)-1])
+        ax.plot(res[m2], res[m1], 'x', color='#53777A')
+        correlations[glom].append(np.corrcoef(res[m2], res[m1])[0, 1])
+        ax.plot([0, 1], [0, 1], color='0.5')
+        ax.set_xlim([-0.2, 1])
+        ax.set_ylim([-0.2, 1])
+        if mn.index(m1) == (mn.index(m2)-1):
+            ax.set_ylabel(m1)
+        if mn.index(m1) == 0:
+            ax.set_title(m2)
+        if not (mn.index(m1) == 0 and mn.index(m2) == 1):
+            ax.set_yticks([])
+        if not (mn.index(m1) == (len(mn)-2) and mn.index(m2) == (len(mn)-1)):
+            ax.set_xticks([])
+    plt.savefig(os.path.join(outpath, glom + '_prediction_comparison.png'))
+
+    if utils.run_from_ipython():
+        plt.show()
+
+    with open(os.path.join(outpath, glom + '_predictions.csv'), 'w') as f:
+        f.write(',{}\n'.format(','.join(res.keys())))
+        for i, molid in enumerate(mol_intersection):
+            f.write(molid + ',')
+            f.write(','.join([str(r[i]) for r in res.values()]))
+            f.write('\n')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.hist(list(it.chain(*correlations.values())))
+fig.savefig(os.path.join(outpath, 'prediction_correlations.png'))
