45f3d45c1bfb53b44a591a2408c1aa22a1097aa5
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-01 13:41:22 +0100

    investigate EVA, how can the sigma 100 eva work that good?
    see 31.01.2013 - 19:42 for details

--- analysis/plot/plot_eva.py
+++ analysis/plot/plot_eva.py
@@ -14,8 +14,11 @@ import numpy as np
 import pylab as plt
 from master.libs import features_lib as fl
 from master.libs import utils
+from master.libs import run_lib
+from scipy.spatial.distance import pdist,squareform
 reload(utils)
 
+glomeruli = ['Or43b', 'Or22a', 'Or19a', 'Or35a']
 molid = '1'
 outpath = '/Users/dedan/projects/master/results/visualization/'
 data_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
@@ -47,3 +50,50 @@ for i, k_width in enumerate([2, 20, 50, 100]):
     utils.simple_axis(ax)
     ax.set_yticks([])
 fig.savefig(os.path.join(outpath, 'eva.png'))
+
+
+for glom in glomeruli:
+    config = {
+        "glomerulus": glom,      # good performance for vib100
+        "data_path": data_path
+    }
+
+    features = fl.get_spectral_features(spectra,
+                                        use_intensity=False,
+                                        kernel_widths=100,
+                                        bin_width=150)
+    features = fl.normalize_features(features)
+    data, targets, molids = run_lib.load_data_targets(config, features)
+    ligands = [m for i, m in enumerate(molids) if targets[i] >= 0.5]
+    inactive = [m for i, m in enumerate(molids) if targets[i] < 0.2]
+
+    ligands_matrix = np.array([features[m] for m in ligands])
+    inactive_matrix = np.array([features[m] for m in inactive])
+    num_lig = ligands_matrix.shape[0]
+    most_simil = np.argmin(squareform(pdist(np.vstack((ligands_matrix, inactive_matrix))))[num_lig:,:num_lig],0)
+    inactive_matrix = inactive_matrix[most_simil, :]
+
+    N = ligands_matrix.shape[1]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(np.mean(ligands_matrix, axis=0), 'r')
+    ax.fill_between(range(N),
+                     np.mean(ligands_matrix, axis=0),
+                     np.max(ligands_matrix, axis=0),
+                     facecolor='r', alpha=0.3)
+    ax.fill_between(range(N),
+                     np.mean(ligands_matrix, axis=0),
+                     np.min(ligands_matrix, axis=0),
+                     facecolor='r', alpha=0.3)
+
+    plt.plot(np.mean(inactive_matrix, axis=0), 'b')
+    ax.fill_between(range(N),
+                     np.mean(inactive_matrix, axis=0),
+                     np.max(inactive_matrix, axis=0),
+                     facecolor='b', alpha=0.3)
+    ax.fill_between(range(N),
+                     np.mean(inactive_matrix, axis=0),
+                     np.min(inactive_matrix, axis=0),
+                     facecolor='b', alpha=0.3)
+    fig.savefig(os.path.join(outpath, glom + '_eva_investigation.png'))
+plt.show()
