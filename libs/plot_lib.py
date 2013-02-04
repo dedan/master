0b6942e78de4aa00d18465fd80e501ac3349b1bf
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-04 17:56:31 +0100

    remove old descriptor performance plot

--- libs/plot_lib.py
+++ libs/plot_lib.py
@@ -121,52 +121,6 @@ def new_descriptor_performance_plot(fig, max_overview, sc, glomeruli=[], boxplot
             if plot_x == 1:
                 ax.set_ylabel('average descriptor score')
 
-
-def descriptor_performance_plot(fig, max_overview, sc):
-    """compare performance of different descriptors for several glomeruli"""
-    for i_meth, method in enumerate(max_overview):
-
-        for i_sel, selection in enumerate(max_overview[method]):
-
-            desc_names = max_overview[method][selection]['desc_names']
-            data = max_overview[method][selection]['max']
-            sort_x = np.argsort(np.mean(data, axis=0))
-            sort_y = np.argsort(np.mean(data, axis=1))
-            data = data[sort_y, :]
-            data = data[:, sort_x]
-
-            plot_x = (i_sel * len(max_overview) + i_meth + 1) * 3 - 2
-            ax1 = fig.add_subplot(6, 3, plot_x)
-            ax1.imshow(data, interpolation='nearest')
-            ax1.set_xticks(range(len(sc['glomeruli'])))
-            ax1.set_xticklabels([sc['glomeruli'][i] for i in sort_x], rotation='45')
-            ax1.set_aspect('auto')
-            ax1.set_title('{}_{}.'.format(method, selection))
-
-            ax = fig.add_subplot(6, 3, plot_x + 1, sharey=ax1)
-            ax.barh(np.arange(len(desc_names)) - 0.5,
-                   np.mean(data, axis=1),
-                   xerr=np.std(data, axis=1), height=0.7)
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.get_xaxis().tick_bottom()
-            ax.get_yaxis().tick_left()
-            ax1.set_yticks(range(len(desc_names)))
-            ax1.set_yticklabels([desc_names[i] for i in sort_y])
-            ax.set_xlabel('average descriptor score')
-            plt.setp(ax.get_yticklabels(), visible=False)
-
-            ax = fig.add_subplot(6, 3, plot_x + 2)
-            bins = np.arange(0, 1, 0.05)
-            ax.hist(data.flatten(), bins=bins, color='b')
-            ax.set_xlim([0, 1])
-            ax.set_xlabel('overall method score histogram')
-            plt.hold(True)
-            ax.hist(data[-3:,:].flatten(), bins=bins, color='r')
-            ax.set_xlim([0, 1])
-            ax.set_xlabel('overall method score histogram')
-            fig.subplots_adjust(hspace=0.35, wspace=0.02)
-
 def plot_search_matrix(fig, desc_res, sc, glomeruli):
     """docstring for plot_search_matrix"""
     methods = desc_res.values()[0].values()[0].keys()
