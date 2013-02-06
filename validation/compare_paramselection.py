16ecb55e4a647e07cdd3b0fff188403c4bf820db
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-06 16:13:10 +0100

    combine paramselection plots for SVR and RFR and make it a bit nicer

--- validation/compare_paramselection.py
+++ validation/compare_paramselection.py
@@ -21,10 +21,12 @@ import pylab as plt
 from master.libs import read_data_lib as rdl
 from master.libs import utils
 
-inpath = '/Users/dedan/projects/master/results/new_param_search/conv_features'
-method = 'svr'
-selection = 'linear'
+inpath = '/Users/dedan/projects/master/results/param_search/forest'
+method = 'forest'
+selection = 'forest'
 
+data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
+used_glomeruli = json.load(open(os.path.join(data_path, 'used_glomeruli.json')))
 search_res, max_overview, sc, k_best_dict = rdl.read_paramsearch_results(inpath)
 
 out_res = {}
@@ -33,20 +35,29 @@ for i, descriptor in enumerate(search_res):
     res = json.load(open(os.path.join(inpath, descriptor + '.json')))
 
     # param selection values to compare against
-    c_k_best = np.max(res['sc']['k_best'])
-    c_regularization = 1.0
-    c_reg_idx = sc[method].index(c_regularization)
+    if method == 'forest':
+        sc[method] = res['sc'][method]
+        sc['svr'] = res['sc']['svr']
+        c_k_best = np.max(sc['k_best'])
+        c_reg_idx = np.min((len(sc[method])-1, len(sc['svr'])-1))
+    else:
+        c_k_best = np.max(res['sc']['k_best'])
+        c_regularization = 1.0
+        c_reg_idx = sc[method].index(c_regularization)
 
     best_genscore = []
     picked_genscore = []
-    for glom in max_overview[method][selection]['glomeruli']:
+    for glom in used_glomeruli:
         best_params = rdl.get_best_params(max_overview, sc, k_best_dict,
                                           descriptor, glom, method, selection)
         k_best = best_params['feature_selection']['k_best']
         reg_idx = res['sc'][method].index(best_params['methods'][method]['regularization'])
         best_res = res['res'][selection][glom][str(k_best)][str(reg_idx)]
         best_genscore.append(best_res[method]['gen_score'])
-        picked_res = res['res'][selection][glom][str(c_k_best)][str(c_reg_idx)]
+        if method == 'forest':
+            picked_res = res['res'][selection][glom][str(k_best)][str(c_reg_idx)]
+        else:
+            picked_res = res['res'][selection][glom][str(c_k_best)][str(c_reg_idx)]
         picked_genscore.append(picked_res[method]['gen_score'])
     out_res[descriptor] = {'best_genscore': best_genscore,
                            'picked_genscore': picked_genscore,
@@ -69,19 +80,23 @@ for i, (descriptor, results) in enumerate(sorted_out_res):
     ax.set_yticks([0, 0.5, 0]) if (i + 1) % n_sub == 1 else ax.set_yticks([])
     ax.set_ylim([0, 1])
 fig.subplots_adjust(hspace=0.7)
-fig.savefig(os.path.join(inpath, 'plots', 'param_selection_comp.png'))
+fig.savefig(os.path.join(inpath, 'plots', method + '_param_selection_comp.png'))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 all_best_genscores = __builtin__.sum([r['best_genscore'] for r in out_res.values()], [])
 all_picked_genscores = __builtin__.sum([r['picked_genscore'] for r in out_res.values()], [])
-ax.plot(all_best_genscores, all_picked_genscores, '.')
+ax.plot(all_best_genscores, all_picked_genscores, 'o', alpha=0.6, markersize=4)
 ax.plot([0, 0.8], [0, 0.8], color='0.5')
 ax.plot([0.1, 0.9], [0, 0.8], color='0.5')
+ax.set_xlabel('param search results')
+ax.set_ylabel('fixed parameter')
+ax.set_title(method)
+utils.simple_axis(ax)
 
-ax.set_xlim([0.3, 1])
-ax.set_ylim([-1, 1])
-fig.savefig(os.path.join(inpath, 'plots', 'param_selection_overview.png'))
+ax.set_xlim([0, 0.8])
+ax.set_ylim([0, 0.8])
+fig.savefig(os.path.join(inpath, 'plots', method + '_param_selection_overview.png'))
 plt.show()
 
 
