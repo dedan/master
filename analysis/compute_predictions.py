44b4417ea49ea862a1e27a50eaae078a316a1a01
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-08 16:01:45 +0100

    finish how predictions are computed and store them in a pickle for later plotting

--- analysis/compute_predictions.py
+++ analysis/compute_predictions.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 '''
 
-    compare the predictions of different models for one glomerulus
+compute predictions to compare the different models for one glomerulus
 
 Created by  on 2012-01-27.
 Copyright (c) 2012. All rights reserved.
@@ -10,83 +10,98 @@ Copyright (c) 2012. All rights reserved.
 import sys
 import os
 import json
+import pickle
 import itertools as it
 from master.libs import run_lib as rl
 from master.libs import utils
 import numpy as np
-import pylab as plt
-import matplotlib.gridspec as gridspec
-from scipy import stats
 reload(rl)
 
-plt.close('all')
 outpath = '/Users/dedan/projects/master/results/predict'
 data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
-configs = json.load(open(sys.argv[1]))
-if len(sys.argv) < 3:
-    gloms = json.load(open(os.path.join(data_path, 'used_glomeruli.json')))
-else:
-    gloms = [sys.argv[2]]
+gloms = json.load(open(os.path.join(data_path, 'all_glomeruli.json')))
 
-correlations = {g: [] for g in gloms}
+base_config = {
+    "data_path": data_path,
+    "feature_selection": {
+        "k_best": "max",
+        "method": "linear"
+        },
+    "features": {
+      "normalize": True,
+      "properties_to_add": []
+    },
+    "methods": {
+      "svr": {
+        "C": 1.0,
+        "n_folds": 50
+      }
+    },
+    "randomization_test": False
+}
+
+feat_config = {
+    "haddad": {
+        "type": "conventional",
+        "descriptor": "haddad_desc",
+    },
+    "saito": {
+        "type": "conventional",
+        "descriptor": "saito_desc",
+    },
+    "all": {
+      "type": "conventional",
+      "descriptor": "all",
+    },
+    "eva": {
+      "type": "spectral",
+      "kernel_width": 100,
+      "bin_width": 150,
+      "use_intensity": False,
+      "spec_type": "ir",
+    }
+}
+method = base_config['methods'].keys()[0]
+
+
+# compute molecules available for all descriptors
+cache = {}
+for k, config in feat_config.items():
+    base_config['features'].update(config)
+    cache[k] = {"features": rl.prepare_features(base_config)}
+all_mols = [r['features'].keys() for r in cache.values()]
+mol_intersection = set(all_mols[0]).intersection(*all_mols[1:])
+
+res = {g: {n: {} for n in feat_config} for g in gloms}
 for glom in gloms:
-    for config in configs.values():
-        config.update({'glomerulus': glom, 'data_path': data_path})
+    print('{}\n'.format(glom))
+    base_config.update({'glomerulus': glom, 'data_path': data_path})
 
-    res = {k: {'features': rl.prepare_features(config)} for k, config in configs.items()}
-    all_mols = [r['features'].keys() for r in res.values()]
-    mol_intersection = set(all_mols[0]).intersection(*all_mols[1:])
+    dtm = {}
+    for name, config in feat_config.items():
+        base_config['features'].update(config)
+        data, targets, molids = rl.load_data_targets(base_config, cache[name]['features'])
+        dtm[name] = {
+            'data': data,
+            'targets': targets,
+            'molids': molids
+        }
 
-    for conf_name, config in configs.items():
-        print('working on model: {}'.format(conf_name))
+    # select molecules that none of the models will be trained on
+    all_trained = set(dtm.values()[0]['molids']).union(*[m['molids'] for m in dtm.values()[1:]])
+    to_predict_molids = mol_intersection - all_trained
 
-        # load features
-        print 'preparing features..'
-        data, targets, molids = rl.load_data_targets(config, res[conf_name]['features'])
-        if config['feature_selection']['k_best'] == 'max':
-            config['feature_selection']['k_best'] = data.shape[1]
+    for name, data in dtm.items():
 
         # fit model
-        print("use {} molecules for training".format(data.shape[0]))
-        tmp_res = rl.run_runner(config, data, targets, get_models=True)
-        method = config['methods'].keys()[0]
-        to_predict = np.array([res[conf_name]['features'][molid] for molid in mol_intersection])
-        res[conf_name] = tmp_res[method]['model'].predict(to_predict)
+        print('working on model: {}'.format(name))
+        base_config['feature_selection']['k_best'] = data['data'].shape[1]
+        print("use {} molecules for training".format(data['data'].shape[0]))
+        tmp_res = rl.run_runner(base_config, data['data'], data['targets'], get_models=True)
+        to_predict = np.array([cache[name]['features'][molid] for molid in to_predict_molids])
+        res[glom][name]['predictions'] = tmp_res[method]['model'].predict(to_predict)
+        res[glom][name]['targets'] = data['targets']
+        res[glom][name]['score'] = tmp_res[method]['gen_score']
         print('model genscore: {:.2f}\n'.format(tmp_res[method]['gen_score']))
 
-
-    mn = configs.keys()
-    gs = gridspec.GridSpec(len(mn)-1, len(mn)-1)
-    gs.update(wspace=0.2, hspace=0.2)
-    for m1, m2 in it.combinations(mn, 2):
-        ax = plt.subplot(gs[mn.index(m1), mn.index(m2)-1])
-        ax.plot(res[m2], res[m1], 'x', color='#53777A')
-        correlations[glom].append(np.corrcoef(res[m2], res[m1])[0, 1])
-        ax.plot([0, 1], [0, 1], color='0.5')
-        ax.set_xlim([-0.2, 1])
-        ax.set_ylim([-0.2, 1])
-        if mn.index(m1) == (mn.index(m2)-1):
-            ax.set_ylabel(m1)
-        if mn.index(m1) == 0:
-            ax.set_title(m2)
-        if not (mn.index(m1) == 0 and mn.index(m2) == 1):
-            ax.set_yticks([])
-        if not (mn.index(m1) == (len(mn)-2) and mn.index(m2) == (len(mn)-1)):
-            ax.set_xticks([])
-    plt.savefig(os.path.join(outpath, glom + '_prediction_comparison.png'))
-
-    if utils.run_from_ipython():
-        plt.show()
-
-    with open(os.path.join(outpath, glom + '_predictions.csv'), 'w') as f:
-        f.write(',{}\n'.format(','.join(res.keys())))
-        for i, molid in enumerate(mol_intersection):
-            f.write(molid + ',')
-            f.write(','.join([str(r[i]) for r in res.values()]))
-            f.write('\n')
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.hist(list(it.chain(*correlations.values())))
-fig.savefig(os.path.join(outpath, 'prediction_correlations.png'))
-print stats.percentileofscore(all_cors, 0.7)
+pickle.dump(dict(res), open(os.path.join(outpath, 'predictions.pkl'), 'w'))
