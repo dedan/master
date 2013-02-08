cf00dff5cf706bc23cd06128ccfffa8ab5289d20
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-08 09:45:42 +0100

    merge results only of file to merge information in already exists

--- scripts/merge_paramsearch_results.py
+++ scripts/merge_paramsearch_results.py
@@ -33,15 +33,15 @@ for new_file in new_files:
     old_res_file = os.path.join(old, os.path.basename(new_file))
     if os.path.exists(old_res_file):
         old_res = json.load(open(old_res_file))
+        new_res = json.load(open(new_file))
+
+        old_res['sc_{}'.format(os.path.basename(new_file))] = new_res['sc']
+        old_rud = RUDict(old_res['res'])
+        old_rud.update(new_res['res'])
+        old_res['res'] = dict(old_rud)
+
+        json.dump(old_res, open(old_res_file, 'w'))
     else:
         print '{} missing'.format(old_res_file)
         old_res = {'res': {}}
-    new_res = json.load(open(new_file))
-
-    old_res['sc_{}'.format(os.path.basename(new_file))] = new_res['sc']
-    old_rud = RUDict(old_res['res'])
-    old_rud.update(new_res['res'])
-    old_res['res'] = dict(old_rud)
-
-    json.dump(old_res, open(old_res_file, 'w'))
 
