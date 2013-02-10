a9cb8d9562aeb8a6a2dc6afd13779e013b4bbcae
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-10 16:50:33 +0100

    common y axis in predictions plot and line at 10th percentile

--- analysis/plot/plot_predictions.py
+++ analysis/plot/plot_predictions.py
@@ -13,6 +13,7 @@ import pickle
 import numpy as np
 import pylab as plt
 from master.libs import utils
+from scipy import stats
 
 inpath = '/Users/dedan/projects/master/results/predict/'
 reference = 'all'
@@ -22,26 +23,33 @@ to_compare = set(res.keys()).difference([reference])
 fig = plt.figure()
 xticks = np.arange(0, 1.1, 0.25)
 xticklabels = ['0', '', '0.5', '', '1']
-for i, desc in enumerate(to_compare):
+bins = np.arange(0, 1.01, 0.05)
+plot_res = {desc:{'corr_both_pos': [], 'corr_one_neg': [], 'all': []} for desc in to_compare}
+for desc in to_compare:
 
-    ax = fig.add_subplot(2, len(to_compare), i+1)
-    correlations_both_pos = []
-    correlations_one_neg = []
     for glom in res[reference]:
-        corr = np.corrcoef(res[reference][glom]['predictions'],
-                           res[desc][glom]['predictions'])[0, 1]
+        corr, _ = stats.pearsonr(res[reference][glom]['predictions'],
+                                 res[desc][glom]['predictions'])
+        plot_res[desc]['all'].append(corr)
         if res[reference][glom]['score'] <= 0 or res[desc][glom]['score'] <=0:
-            correlations_one_neg.append(corr)
+            plot_res[desc]['corr_one_neg'].append(corr)
         else:
-            correlations_both_pos.append(corr)
-    bins = np.arange(0, 1.01, 0.05)
-    c_both, _ = np.histogram(correlations_both_pos, bins=bins)
+            plot_res[desc]['corr_both_pos'].append(corr)
+
+ylim = np.max([np.histogram(v['all'], bins=bins)[0] for v in plot_res.values()])
+for i, (desc, pres) in enumerate(plot_res.items()):
+    ax = fig.add_subplot(2, len(to_compare), i+1)
+    c_both, _ = np.histogram(pres['corr_both_pos'], bins=bins)
     plt.bar(bins[:-1], c_both, width=bins[1]-bins[0], color='0.5')
-    c_one, _ = np.histogram(correlations_one_neg, bins=bins)
+    c_one, _ = np.histogram(pres['corr_one_neg'], bins=bins)
     plt.bar(bins[:-1], c_one, bottom=c_both, width=bins[1]-bins[0], color='0.8')
-    annotation_text = '{}/{}'.format(len(correlations_both_pos), len(res[reference]))
+    annotation_text = '{}/{}'.format(len(pres['corr_both_pos']), len(res[reference]))
     ax.text(0.1, 0.9, annotation_text, transform=ax.transAxes)
+    scor_perc = stats.scoreatpercentile(pres['all'], 10)
+    print desc, scor_perc
+    ax.plot([scor_perc, scor_perc], [-1, ylim*2/3], 'r')
     ax.set_xlim([0, 1])
+    ax.set_ylim([0, ylim])
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels)
     if i == 0:
@@ -67,3 +75,4 @@ for i, desc in enumerate(to_compare):
     ax.set_xlim([0, 1])
     ax.set_ylim([0, 1])
     utils.simple_axis(ax)
+fig.savefig(os.path.join(inpath, 'predictions_comparison.png'))
