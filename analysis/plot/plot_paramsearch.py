09028437f3a0772e103b8828f6a3014d4b8609d2
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-11 17:47:42 +0100

    remove descriptor performance comparison
    this plot moved to the predictions plot

--- analysis/plot/plot_paramsearch.py
+++ analysis/plot/plot_paramsearch.py
@@ -48,31 +48,6 @@ fig.savefig(os.path.join(outpath, ptype + '_desc_comparison.' + config['format']
 plt.show()
 
 
-# descriptor comparison plot for svr lin
-desc2comp = ['haddad_desc', 'saito_desc', 'all', 'vib_100']
-mn = desc2comp
-gs = gridspec.GridSpec(len(mn)-1, len(mn)-1)
-gs.update(wspace=0.2, hspace=0.2)
-cur_max = max_overview['svr']['linear']
-for m1, m2 in it.combinations(mn, 2):
-    ax = plt.subplot(gs[mn.index(m1), mn.index(m2)-1])
-    desc_idx1 = cur_max['desc_names'].index(m1)
-    desc_idx2 = cur_max['desc_names'].index(m2)
-    ax.plot(cur_max['p_selection'][desc_idx2, :], cur_max['p_selection'][desc_idx1, :], 'kx')
-    ax.plot([0, 1], [0, 1], color='0.5')
-    ax.axis('equal')
-    ax.set_xlim([0, 1])
-    ax.set_ylim([0, 1])
-    if mn.index(m1) == (mn.index(m2)-1):
-        ax.set_ylabel(m1)
-    if mn.index(m1) == 0:
-        ax.set_title(m2)
-    if not (mn.index(m1) == 0 and mn.index(m2) == 1):
-        ax.set_yticks([])
-    if not (mn.index(m1) == (len(mn)-2) and mn.index(m2) == (len(mn)-1)):
-        ax.set_xticks([])
-plt.savefig(os.path.join(outpath, 'descriptor_comparison_forest.png'))
-
 # ML method comparison plot
 markers = ['1', '0']
 desc2comp = ['vib_100', 'all']
