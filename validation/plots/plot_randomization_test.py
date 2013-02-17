19b1cdf4dd60e2329d9611235fa6842123c05dd5
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-17 11:52:06 +0100

    change the randomization plot to only give an overview over all tests and glomeruli in a single scatter plot

--- validation/plots/plot_randomization_test.py
+++ validation/plots/plot_randomization_test.py
@@ -16,48 +16,42 @@ import pylab as plt
 from master.libs import read_data_lib as rdl
 from master.libs import utils
 
-res = {}
 method = 'forest'
-inpath = '/Users/dedan/projects/master/results/validation/svrlin_haddad'
+inpath = '/Users/dedan/projects/master/results/validation/'
 
-outpath = os.path.join(inpath, 'plots')
-if not os.path.exists(outpath):
-    os.mkdir(outpath)
+if not os.path.exists(os.path.join(inpath, 'collected_res.json')):
 
-true_res = json.load(open(os.path.join(inpath, 'true.json')))
-for glom, value in true_res.items():
-    if not glom in res:
-        res[glom] = {"rand_res_dist": []}
-    res[glom]['true_res'] = value
+    res = {d: {} for d in os.listdir(inpath) if os.path.isdir(os.path.join(inpath, d))}
+    for subdir in res:
+        true_res = json.load(open(os.path.join(inpath, subdir, 'true.json')))
+        for glom, value in true_res.items():
+            if not glom in res:
+                res[subdir][glom] = {"rand_res_dist": []}
+            res[subdir][glom]['true_res'] = value
+        rand_res_files = glob.glob(os.path.join(inpath, subdir, 'run_*.json'))
+        for rand_res_file in rand_res_files:
+            rand_res = json.load(open(rand_res_file))
+            for glom, value in rand_res.items():
+                res[subdir][glom]['rand_res_dist'].append(value)
+        # compute statistics
+        for r in res[subdir].values():
+            sum_larger_true = np.sum(np.array(r['rand_res_dist']) > r['true_res'])
+            n_repetitions = float(len(r['rand_res_dist']))
+            r['p'] = (1 + sum_larger_true) / n_repetitions
+    json.dump(res, open(os.path.join(inpath, 'collected_res.json'), 'w'))
+else:
+    res = json.load(open(os.path.join(inpath, 'collected_res.json')))
 
-rand_res_files = glob.glob(os.path.join(inpath, 'run_*.json'))
-for rand_res_file in rand_res_files:
-    rand_res = json.load(open(rand_res_file))
-    for glom, value in rand_res.items():
-        res[glom]['rand_res_dist'].append(value)
 
 
-# compute statistics
-for r in res.values():
-    sum_larger_true = np.sum(np.array(r['rand_res_dist']) > r['true_res'])
-    n_repetitions = float(len(r['rand_res_dist']))
-    r['p'] = (1 + sum_larger_true) / n_repetitions
-
-# create a plot of the resulting distribution and the original value
-fig = plt.figure(figsize=(8,4))
-for i, glom in enumerate(res):
-
-    ax = fig.add_subplot(len(res), 1, i+1)
-    ax.hist(res[glom]['rand_res_dist'], color='0.5')
-    ax.plot([res[glom]['true_res']], [20], 'r.')
-    ax.set_ylabel('{} p: {:.3f}'.format(glom, res[glom]['p']), rotation='0')
-    ax.set_yticks([])
-    ax.set_xlim([-0.4, 0.8])
-    if not i == len(res) - 1:
-        ax.set_xticks([])
-    print('{} - p: {:.5f}'.format(glom, res[glom]['p']))
-fig.subplots_adjust(hspace=0.3)
-fig.savefig(os.path.join(outpath, 'rand_test.png'))
+fig = plt.figure()
+ax = fig.add_subplot(111)
+for i, (subdir, tmp_res) in enumerate(res.items()):
+    plt.plot([r['true_res'] for r in tmp_res.values() if r['true_res'] > 0],
+             [r['p'] for r in tmp_res.values() if r['true_res'] > 0],
+             'ko')
+    ax.set_title(subdir)
+fig.savefig(os.path.join(inpath, 'rand_test_overview.png'))
 if utils.run_from_ipython():
     plt.show()
 
