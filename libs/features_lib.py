fix a bug in spectral feature computation
I wanted to validate that the application of raman and IR intensities is done correctly and realized that always k_width values are the same if the bin_width is small than k_width. This was caused by a missing float() casting. Should not have any influence on the results because we always used bin_width = 1.5 sigma

libs/features_lib.py
@@ -91,12 +91,12 @@ def get_features_for_molids(f_space, molids):
 
 def _gaussian(x, mu, sigma):
     """docstring for gaussian"""
-    return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x-mu)/sigma)**2)
+    return 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x-mu)/float(sigma))**2)
 
 def _sum_of_gaussians(x_range, positions, heights, sigma):
     """docstring for sum_of_gaussians"""
     assert len(positions) == len(heights)
-    return [np.sum([heights * _gaussian(x, positions, sigma)]) for x in x_range]
+    return [np.sum(heights * _gaussian(x, positions, sigma)) for x in x_range]
 
 def get_spectral_features(spectra, use_intensity=True, spec_type='ir',
                           kernel_widths=10, bin_width=10, BFS_max=4000):

tests/test_libs.py
@@ -84,7 +84,16 @@ class TestLearning(unittest.TestCase):
 
 class TestFlib(unittest.TestCase):
     """test my feature lib"""
-    pass
+
+    def test_spectral_features(self):
+        """docstring for test_spectral_features"""
+        bin_width = 1
+        k_width = 5
+        spectra = {'1': {'freq': np.array([20, 30, 50]), 'ir': np.array([20, 1, 1])}}
+        x_range = range(0, 10, bin_width)
+        intensities = np.ones(len(spectra['1']['ir']))
+        tmp = flib._sum_of_gaussians(x_range, spectra['1']['freq'], intensities, k_width)
+        self.assertTrue(tmp[0] != tmp[1])
 
 class TestLibs(unittest.TestCase):
 