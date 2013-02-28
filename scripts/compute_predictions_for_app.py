5c2745442f74a0ccd8934f59561ec1dcdba73485
Stephan Gabler <stephan.gabler@gmail.com>
2013-02-28 17:35:22 +0100

    add a script to compute predictions for the odor_app

---
+++ scripts/compute_predictions_for_app.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+compute the predictions and save them for later usage
+
+depends on the code from my master thesis
+
+    github.com/dedan/master
+
+Created by  on 2012-01-27.
+Copyright (c) 2012. All rights reserved.
+"""
+
+import sys
+import os
+
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+
+compute predictions to compare the different models for one glomerulus
+
+Created by  on 2012-01-27.
+Copyright (c) 2012. All rights reserved.
+'''
+import sys
+import os
+import json
+import pickle
+import itertools as it
+from master.libs import run_lib as rl
+from master.libs import utils
+import numpy as np
+reload(rl)
+
+outpath = os.path.join(os.path.dirname(__file__), '..', '..', 'models')
+data_path = os.path.join(os.path.dirname(__file__), '..', 'data')
+door2id = json.load(open(os.path.join(data_path, 'door2id.json')))
+id2door = {v[0]: k for k, v in door2id.items()}
+gloms = json.load(open(os.path.join(data_path, 'all_glomeruli.json')))
+gloms = gloms[:2]
+
+base_config = {
+    "data_path": data_path,
+    "feature_selection": {
+        "k_best": "max",
+        "method": "linear"
+        },
+    "features": {
+      "normalize": True,
+      "properties_to_add": []
+    },
+    "methods": {
+      "svr": {
+        "C": 1.0,
+        "n_folds": 50
+      }
+    },
+    "randomization_test": False
+}
+
+feat_config = {
+    "haddad": {
+        "type": "conventional",
+        "descriptor": "haddad_desc",
+    },
+    "all": {
+      "type": "conventional",
+      "descriptor": "all",
+    },
+    "eva_100": {
+      "type": "spectral",
+      "kernel_width": 100,
+      "bin_width": 150,
+      "use_intensity": False,
+      "spec_type": "ir",
+    }
+}
+method = base_config['methods'].keys()[0]
+
+
+# compute molecules available for all descriptors
+cache = {}
+for k, config in feat_config.items():
+    base_config['features'].update(config)
+    cache[k] = {"features": rl.prepare_features(base_config)}
+all_mols = [r['features'].keys() for r in cache.values()]
+mol_intersection = set(all_mols[0]).intersection(*all_mols[1:])
+
+res = {n: {g: {} for g in gloms} for n in feat_config}
+for glom in gloms:
+    print('{}\n'.format(glom))
+    base_config.update({'glomerulus': glom, 'data_path': data_path})
+
+    dtm = {}
+    for name, config in feat_config.items():
+        base_config['features'].update(config)
+        data, targets, molids = rl.load_data_targets(base_config, cache[name]['features'])
+        dtm[name] = {
+            'data': data,
+            'targets': targets,
+            'molids': molids
+        }
+
+    # select molecules that none of the models will be trained on
+    all_trained = set(dtm.values()[0]['molids']).union(*[m['molids'] for m in dtm.values()[1:]])
+    to_predict_molids = mol_intersection - all_trained
+
+    for name, data in dtm.items():
+
+        # fit model
+        print('working on model: {}'.format(name))
+        base_config['feature_selection']['k_best'] = data['data'].shape[1]
+        print("use {} molecules for training".format(data['data'].shape[0]))
+        tmp_res = rl.run_runner(base_config, data['data'], data['targets'], get_models=True)
+        to_predict = np.array([cache[name]['features'][molid] for molid in to_predict_molids])
+        res[name][glom]['predictions'] = tmp_res[method]['model'].predict(to_predict)
+        res[name][glom]['cases'] = [id2door[molid] for molid in to_predict_molids]
+        res[name][glom]['targets'] = data['targets']
+        res[name][glom]['score'] = tmp_res[method]['gen_score']
+        print('model genscore: {:.2f}\n'.format(tmp_res[method]['gen_score']))
+
+pickle.dump(dict(res), open(os.path.join(outpath, 'predictions.pkl'), 'w'))
+
